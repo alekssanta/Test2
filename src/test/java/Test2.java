9975969a92baafed57b5fa116c71d73d0a8700f1
alekssanta <alekslapin@bk.ru>
2019-09-12 20:31:34 +0400

    12.09.19

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,34 +13,69 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.WebDriver.*;
+// сделать before & after в тесте
 
-public class Test2  {
+
+public class Test2 {
     public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+
+
+//        @After
+//        public void End{
+//if (driver !=null)
+//    driver.quit();
+
+
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://market.yandex.ru/");
-        driver.findElement(By.id("header-search")).clear();
-        driver.findElement(By.id("header-search")).click();
-        driver.findElement(By.id("header-search")).sendKeys("macbook");
 
-        driver.findElement(By.cssSelector(".button2_type_submit")).click();
+        driver = new ChromeDriver();
 
-        driver.findElement(By.className("wishlist-control")).click();
-driver.get("https://market.yandex.ru/my/wishlist?track=rmmbr");
-
-         //driver.findElement(By.xpath("//a[contains(@href, '/my/wishlist?track=rmmbr')]")).click();
-//xpath=//a[contains(@href, '/my/wishlist?track=rmmbr')]  клик по отложенным
+        driver.get("https://market.yandex.ru/"); // открываем страницу браузера
+        driver.findElement(By.id("header-search")).clear(); // очищаеем строку поиска
+        driver.findElement(By.id("header-search")).sendKeys("macbook Pro 15"); // отправляем запрос
         Thread.sleep(2000);
-        driver.quit();
+        driver.findElement(By.cssSelector(".button2_type_submit")).click();  // клацаем по кнопке "найти"
+        Thread.sleep(2000);
+        driver.findElement(By.className("wishlist-control")).click(); // клацаем по кнопке "добавить в отложенные"
+        Thread.sleep(2000);
+        driver.get("https://market.yandex.ru/my/wishlist"); // заходим в "отложенные
 
-    }
-//    private boolean isDisplayedOnPage(WebElementFacade wef){
-//        try{
-//            return wef.isDisplayed();
-//        } catch (NoSuchElementException nsee){
-//            return false;
+        // driver.findElement(By.xpath("//span[text()='₽']"));
+
+
+
+        Thread.sleep(2000);
+
+
+
+//        try {
+//            Assert.assertTrue(driver.findElement(By.xpath("//html")).getText().contains("Macbook Pro 15"));
+//
+//        } catch (Error e) {
+//            System.out.println("Не нашел");
 //        }
-//    }
+
+
+        boolean isPresent = driver.findElements(By.cssSelector(".shop-history__link")).size() >0;
+       // boolean isPresent = driver.findElements(By.xpath("//a[contains(@href,'https://market-click2.yandex.ru/redir/')]")).size() > 0;
+
+        if (isPresent) System.out.println("Товар со ссылкой в отложенных");
+        else {
+            System.out.println("Товара нет");
+        }
+
+//        Boolean checkTitle = driver.findElement(By.xpath("//*[contains(@text(),'Macbook Pro')]")) > 0;
+//        if (checkTitle = true) {
+//            System.out.println("Находится Macbook Pro");
+//        }
+//        else System.out.println("Товар с другим наименованием");
+        driver.quit();
+    }
 
 
 }
+
+
+
